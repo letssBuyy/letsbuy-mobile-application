27fcfb020141fc45a3916360b2a223b0ff51208b
Leonardo Nacagawa Silva <leonardonacasilva@gmail.com>
2023-10-03 23:42:49 -0300

    cadastro de anuncio funfando legal com imagens

--- app/src/main/java/com/example/letsbuy/PublishAdActivity.kt
+++ app/src/main/java/com/example/letsbuy/PublishAdActivity.kt
@@ -1,7 +1,9 @@
 package com.example.letsbuy
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -26,18 +28,21 @@ import com.example.letsbuy.service.AdversimentService
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.File
 
 class PublishAdActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPublishAdBinding
-    private lateinit var imageButton: ImageButton
     private lateinit var selectedColor: String
     private lateinit var selectedCategory: String
     private lateinit var selectedQuality: String
+    private lateinit var imageButton1: ImageButton
+    private lateinit var imageButton2: ImageButton
+    private lateinit var imageButton3: ImageButton
+    private lateinit var imageButton4: ImageButton
+    private var photoCounter = 0
+    private val PICK_MULTIPLE_IMAGES_FROM_GALLERY_REQUEST_CODE = 400
 
-    companion object {
-        val IMAGE_REQUEST_CODE = 100
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -140,23 +145,60 @@ class PublishAdActivity : AppCompatActivity() {
             }
         }
 
-        imageButton = findViewById(R.id.image_button_photo1)
-        imageButton.setOnClickListener {
+        binding.imageButtonPhoto1.setOnClickListener {
             pickImageGallery()
         }
+
+        imageButton1 = binding.imageButtonPhoto1
+        imageButton2 = binding.imageButtonPhoto2
+        imageButton3 = binding.imageButtonPhoto3
+        imageButton4 = binding.imageButtonPhoto4
+
     }
 
     private fun pickImageGallery() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
-        startActivityForResult(intent, IMAGE_REQUEST_CODE)
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        startActivityForResult(
+            Intent.createChooser(intent, "Selecione 4 fotos"),
+            PICK_MULTIPLE_IMAGES_FROM_GALLERY_REQUEST_CODE
+        )
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK) {
-            imageButton.setImageURI(data?.data)
+        if(requestCode == PICK_MULTIPLE_IMAGES_FROM_GALLERY_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            if(data.clipData != null) {
+                photoCounter = 0
+                val count = data.clipData!!.itemCount
+                for(i in 0 until count) {
+                    val uri: Uri = data.clipData!!.getItemAt(i).uri
+                    val file = uri.path?.let { File(it) }
+                    file?.name?.let { Log.d("TAG_URI: ", it) }
+                    photoCounter++
+
+                    when(i){
+                        0 -> imageButton1.setImageURI(data.clipData!!.getItemAt(0).uri)
+                        1 -> imageButton2.setImageURI(data.clipData!!.getItemAt(1).uri)
+                        2 -> imageButton3.setImageURI(data.clipData!!.getItemAt(2).uri)
+                        3 -> imageButton4.setImageURI(data.clipData!!.getItemAt(3).uri)
+                    }
+                    if(photoCounter >= 4) {
+                        Toast.makeText(
+                            this,
+                            "Você já selecionou 4 fotos",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                       return
+                    }
+                }
+            } else {
+                val uri: Uri? = data.data
+                val file = uri?.path?.let { File(it) }
+                file?.name?.let { Log.d("TAG_PATH: ", it) }
+            }
         }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     private fun validInputs(
@@ -181,6 +223,13 @@ class PublishAdActivity : AppCompatActivity() {
 
         val ivIconErrorPrice = findViewById<ImageView>(R.id.iv_icon_error_price)
         val tvMessageErrorPrice = findViewById<TextView>(R.id.tv_message_error_price)
+
+        val ivIconErrorImage = findViewById<ImageView>(R.id.iv_icon_error_image)
+        val tvMessageErrorImage = findViewById<TextView>(R.id.tv_message_error_image)
+
+        val imageOld1 = binding.imageButtonPhotoOld1.drawable.constantState
+        val imageOld234 = binding.imageButtonPhotoOld234.drawable.constantState
+
 
         if(title.isEmpty()) {
             valid = false
@@ -225,6 +274,20 @@ class PublishAdActivity : AppCompatActivity() {
         } else {
             ivIconErrorPrice.visibility = View.GONE
             tvMessageErrorPrice.visibility = View.GONE
+        }
+
+        if(
+            imageButton1.drawable.constantState!! == imageOld1 ||
+            imageButton2.drawable.constantState!! == imageOld234 ||
+            imageButton3.drawable.constantState!! == imageOld234 ||
+            imageButton4.drawable.constantState!! == imageOld234
+        ) {
+            valid = false
+            ivIconErrorImage.visibility = View.VISIBLE
+            tvMessageErrorImage.visibility = View.VISIBLE
+        } else {
+            ivIconErrorImage.visibility = View.GONE
+            tvMessageErrorImage.visibility = View.GONE
         }
 
         return valid
---
+++ app/src/main/java/com/example/letsbuy/service/ImageService.kt
@@ -0,0 +1,10 @@
+package com.example.letsbuy.service
+
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface ImageService {
+
+    //@POST("/adversiment/9")
+    //fun uploadImages(@Query("img1") img1: Multipart)
+}
