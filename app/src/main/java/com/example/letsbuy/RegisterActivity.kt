9a29dcfd289a1e978b0b6b0f7caefdc66e7e563e
ythudson <ythudson@stefanini.com>
2023-10-28 22:29:59 -0300

    Fix(Register) validation of registration fields

--- app/src/main/java/com/example/letsbuy/RegisterActivity.kt
+++ app/src/main/java/com/example/letsbuy/RegisterActivity.kt
@@ -2,9 +2,7 @@ package com.example.letsbuy
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.letsbuy.api.Rest
@@ -15,9 +13,10 @@ import com.example.letsbuy.service.UserService
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.net.URI
 import java.text.SimpleDateFormat
 import java.time.LocalDate
+import java.time.Period
+import java.time.format.DateTimeFormatter
 import java.util.Locale
 
 class RegisterActivity : AppCompatActivity() {
@@ -37,12 +36,12 @@ class RegisterActivity : AppCompatActivity() {
         }
 
         binding.btnConcluirCadastro.setOnClickListener {
-            val name = findViewById<EditText>(R.id.edit_name).text.toString()
-            val email = findViewById<EditText>(R.id.edit_email).text.toString()
-            val cpf = findViewById<EditText>(R.id.edit_cpf).text.toString()
-            val password = findViewById<EditText>(R.id.edit_password).text.toString()
-            val birthDate = findViewById<EditText>(R.id.edit_birth_date).text.toString()
-            val phoneNumber = findViewById<EditText>(R.id.edit_phone).text.toString()
+            val name = binding.editName.text.toString()
+            val email = binding.editEmail.text.toString()
+            val cpf = binding.editCpf.unMasked
+            val password = binding.editPassword.text.toString()
+            val birthDate = binding.editBirthDate.text.toString()
+            val phoneNumber = binding.editPhone.unMasked
 
             val isValid = validInputs(name, email, cpf, password, birthDate, phoneNumber)
 
@@ -83,7 +82,7 @@ class RegisterActivity : AppCompatActivity() {
         val api = Rest.getInstance().create(UserService::class.java)
         var register = UserDto(name, email, cpf, password, birthDate, phoneNumber)
 
-        api.createUser(register).enqueue(object: Callback<UserDtoResponse> {
+        api.createUser(register).enqueue(object : Callback<UserDtoResponse> {
 
             override fun onResponse(
                 call: Call<UserDtoResponse>,
@@ -137,60 +136,185 @@ class RegisterActivity : AppCompatActivity() {
         val ivIconErrorPhoneNumber = binding.ivIconErrorPhone
         val tvMessageErrorPhoneNumber = binding.tvMessageErrorPhone
 
-        if (name.isEmpty()) {
-            valid = false
-            ivIconErrorName.visibility = View.VISIBLE
-            tvMessageErrorName.visibility = View.VISIBLE
-        } else {
-            ivIconErrorName.visibility = View.GONE
-            tvMessageErrorName.visibility = View.GONE
+        when {
+            name.isEmpty() -> {
+                valid = false
+                ivIconErrorName.visibility = View.VISIBLE
+                tvMessageErrorName.visibility = View.VISIBLE
+            }
+
+            name.length > 50 -> {
+                valid = false
+                ivIconErrorName.visibility = View.VISIBLE
+                tvMessageErrorName.visibility = View.VISIBLE
+                tvMessageErrorName.text = "Deve ser menor que 51 caracteres"
+            }
+
+            name.length < 3 -> {
+                valid = false
+                ivIconErrorName.visibility = View.VISIBLE
+                tvMessageErrorName.visibility = View.VISIBLE
+                tvMessageErrorName.text = "Deve ser maior que 2 caracteres"
+            }
+
+            else -> {
+                ivIconErrorName.visibility = View.GONE
+                tvMessageErrorName.visibility = View.GONE
+            }
         }
 
-        if (email.isEmpty()) {
-            valid = false
-            ivIconErrorEmail.visibility = View.VISIBLE
-            tvMessageErrorEmail.visibility = View.VISIBLE
-        } else {
-            ivIconErrorEmail.visibility = View.GONE
-            tvMessageErrorEmail.visibility = View.GONE
+        when {
+            email.isEmpty() -> {
+                valid = false
+                ivIconErrorEmail.visibility = View.VISIBLE
+                tvMessageErrorEmail.visibility = View.VISIBLE
+            }
+
+            email.indexOf("@", 3, false) == -1 -> {
+                valid = false
+                ivIconErrorEmail.visibility = View.VISIBLE
+                tvMessageErrorEmail.visibility = View.VISIBLE
+                tvMessageErrorEmail.text = "Deve possuir @"
+            }
+
+            email.indexOf(".com", 5, false) == -1 -> {
+                valid = false
+                ivIconErrorEmail.visibility = View.VISIBLE
+                tvMessageErrorEmail.visibility = View.VISIBLE
+                tvMessageErrorEmail.text = "Email invalido"
+            }
+
+            else -> {
+                ivIconErrorEmail.visibility = View.GONE
+                tvMessageErrorEmail.visibility = View.GONE
+            }
         }
 
-        if (cpf.isEmpty()) {
-            valid = false
-            ivIconErrorCpf.visibility = View.VISIBLE
-            tvMessageErrorCpf.visibility = View.VISIBLE
-        } else {
-            ivIconErrorCpf.visibility = View.GONE
-            tvMessageErrorCpf.visibility = View.GONE
+        when {
+            cpf.isEmpty() -> {
+                valid = false
+                ivIconErrorCpf.visibility = View.VISIBLE
+                tvMessageErrorCpf.visibility = View.VISIBLE
+            }
+
+            cpf.length != 11 -> {
+                valid = false
+                ivIconErrorCpf.visibility = View.VISIBLE
+                tvMessageErrorCpf.visibility = View.VISIBLE
+                tvMessageErrorCpf.text = "Deve possuir 11 digito"
+            }
+
+            !validateCPF(cpf) -> {
+                valid = false
+                ivIconErrorCpf.visibility = View.VISIBLE
+                tvMessageErrorCpf.visibility = View.VISIBLE
+                tvMessageErrorCpf.text = "CPF invalido"
+            }
+
+            else -> {
+                ivIconErrorCpf.visibility = View.GONE
+                tvMessageErrorCpf.visibility = View.GONE
+            }
         }
 
-        if (password.isEmpty()) {
-            valid = false
-            ivIconErrorPassword.visibility = View.VISIBLE
-            tvMessageErrorPassword.visibility = View.VISIBLE
-        } else {
-            ivIconErrorPassword.visibility = View.GONE
-            tvMessageErrorPassword.visibility = View.GONE
+        when {
+            password.isEmpty() -> {
+                valid = false
+                ivIconErrorPassword.visibility = View.VISIBLE
+                tvMessageErrorPassword.visibility = View.VISIBLE
+            }
+
+            password.length < 6 -> {
+                valid = false
+                ivIconErrorPassword.visibility = View.VISIBLE
+                tvMessageErrorPassword.visibility = View.VISIBLE
+                tvMessageErrorPassword.text = "Senha deve conter no minimo 6 caracteres"
+            }
+
+            password.length > 12 -> {
+                valid = false
+                ivIconErrorPassword.visibility = View.VISIBLE
+                tvMessageErrorPassword.visibility = View.VISIBLE
+                tvMessageErrorPassword.text = "Senha deve conter no maximo 12"
+            }
+
+            else -> {
+                ivIconErrorPassword.visibility = View.GONE
+                tvMessageErrorPassword.visibility = View.GONE
+            }
         }
 
-        if (birthDate.isEmpty()) {
-            valid = false
-            ivIconErrorBirthDate.visibility = View.VISIBLE
-            tvMessageErrorBirthDate.visibility = View.VISIBLE
-        } else {
-            ivIconErrorBirthDate.visibility = View.GONE
-            tvMessageErrorBirthDate.visibility = View.GONE
+        when {
+            birthDate.isEmpty() -> {
+                valid = false
+                ivIconErrorBirthDate.visibility = View.VISIBLE
+                tvMessageErrorBirthDate.visibility = View.VISIBLE
+            }
+
+            calculateAge(birthDate) < 18 -> {
+                valid = false
+                ivIconErrorBirthDate.visibility = View.VISIBLE
+                tvMessageErrorBirthDate.visibility = View.VISIBLE
+                tvMessageErrorBirthDate.text = "Precisa ser maior de idade"
+            }
+
+            else -> {
+                ivIconErrorBirthDate.visibility = View.GONE
+                tvMessageErrorBirthDate.visibility = View.GONE
+            }
         }
 
-        if (phoneNumber.isEmpty()) {
-            valid = false
-            ivIconErrorPhoneNumber.visibility = View.VISIBLE
-            tvMessageErrorPhoneNumber.visibility = View.VISIBLE
-        } else {
-            ivIconErrorPhoneNumber.visibility = View.GONE
-            tvMessageErrorPhoneNumber.visibility = View.GONE
-        }
+        when {
+            phoneNumber.isEmpty() -> {
+                valid = false
+                ivIconErrorPhoneNumber.visibility = View.VISIBLE
+                tvMessageErrorPhoneNumber.visibility = View.VISIBLE
+            }
 
+            phoneNumber.length != 11 -> {
+                valid = false
+                ivIconErrorPhoneNumber.visibility = android.view.View.VISIBLE
+                tvMessageErrorPhoneNumber.visibility = android.view.View.VISIBLE
+                tvMessageErrorPhoneNumber.text = "Numero de telefone invalido"
+            }
+
+            else -> {
+                ivIconErrorPhoneNumber.visibility = View.GONE
+                tvMessageErrorPhoneNumber.visibility = View.GONE
+            }
+        }
         return valid
+    }
+
+    private fun validateCPF(cpf: String): Boolean {
+
+        if (cpf.length != 11 || cpf.toSet().size == 1) {
+            return false
+        }
+
+        val digits = cpf.map { it.toString().toInt() }
+
+        val firstVerifierDigit = calculateVerifierDigit(digits.take(9))
+        val secondVerifierDigit =
+            calculateVerifierDigit(digits.take(9) + listOf(firstVerifierDigit))
+
+        return cpf.substring(9, 11) == "$firstVerifierDigit$secondVerifierDigit"
+    }
+
+    private fun calculateVerifierDigit(digits: List<Int>): Int {
+        var weight = digits.size + 1
+        val sum = digits.fold(0) { acc, digit ->
+            acc + digit * weight--
+        }
+        val remainder = sum % 11
+        return if (remainder < 2) 0 else 11 - remainder
+    }
+
+    private fun calculateAge(birthDate: String): Int {
+        val today = LocalDate.now()
+        val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+        val date = LocalDate.parse(birthDate, formatter)
+        val period = Period.between(date, today)
+        return period.years
     }
 }
