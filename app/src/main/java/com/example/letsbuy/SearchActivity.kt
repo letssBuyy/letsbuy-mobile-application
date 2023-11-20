b577fd78271dad6de09f68333f493a359c81e86c
ythudson <ythudson@stefanini.com>
2023-11-20 18:51:06 -0300

    Fix(Home) criação dos filtros da home e listagem das categorias

---
+++ app/src/main/java/com/example/letsbuy/SearchActivity.kt
@@ -0,0 +1,122 @@
+package com.example.letsbuy
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.letsbuy.adapter.AdapterSearch
+import com.example.letsbuy.api.Rest
+import com.example.letsbuy.databinding.ActivitySearchBinding
+import com.example.letsbuy.dto.AdversimentFilterDto
+import com.example.letsbuy.dto.AllAdversimentsAndLikeDtoResponse
+import com.example.letsbuy.model.enums.AdversimentColorEnum
+import com.example.letsbuy.model.enums.CategoryEnum
+import com.example.letsbuy.model.enums.QualityEnum
+import com.example.letsbuy.service.SearchService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class SearchActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySearchBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val pref = this.getSharedPreferences("AUTH", AppCompatActivity.MODE_PRIVATE)
+        var id = pref?.getString("ID", null)?.toLong()
+
+        binding.imgBack.setOnClickListener {
+            val home = Intent(baseContext, HomeActivity::class.java)
+            startActivity(home)
+        }
+
+        binding.editSearch.setOnEditorActionListener { _, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE ||
+                actionId == EditorInfo.IME_ACTION_SEARCH ||
+                (event != null && event.action == KeyEvent.ACTION_DOWN &&
+                        event.keyCode == KeyEvent.KEYCODE_ENTER)
+            ) {
+                filterSearch(id!!)
+                return@setOnEditorActionListener true
+            }
+            false
+        }
+
+        val bottomFilterFragment = BottomSheetFilterFragment()
+        binding.btnFilter.setOnClickListener {
+            bottomFilterFragment.show(supportFragmentManager, "BottomSheetDialog")
+        }
+
+        filterSearch(id!!)
+    }
+
+    private fun initRecyclerView(listSearch: List<AllAdversimentsAndLikeDtoResponse>) {
+        binding.recyclerViewSearch.layoutManager =
+            GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false)
+        binding.recyclerViewSearch.setHasFixedSize(true)
+        binding.recyclerViewSearch.adapter = AdapterSearch(listSearch)
+    }
+
+    fun filterSearch(idUser: Long) {
+        val api = Rest.getInstance().create(SearchService::class.java)
+
+        val title = binding.editSearch.text.toString()
+
+        val location = if (intent.getStringExtra("LOCATION")
+                .isNullOrEmpty()
+        ) null else intent.getStringExtra("LOCATION")
+        val priceMin = if (intent.getDoubleExtra("PRICEMIN", -1.0)
+                .equals(-1.0)
+        ) null else intent.getDoubleExtra("PRICEMIN", -1.0)
+        val priceMax = if (intent.getDoubleExtra("PRICEMAX", -1.0)
+                .equals(-1.0)
+        ) null else intent.getDoubleExtra("PRICEMAX", -1.0)
+        val quality = if (intent.getStringExtra("QUALITY")
+                .isNullOrEmpty()
+        ) null else QualityEnum.qualityToEnum(intent.getStringExtra("QUALITY")!!)
+        val category = if (intent.getStringExtra("CATEGORY")
+                .isNullOrEmpty()
+        ) null else CategoryEnum.categoryToEnum(intent.getStringExtra("CATEGORY")!!)
+        val color = if (intent.getStringExtra("COLOR")
+                .isNullOrEmpty()
+        ) null else AdversimentColorEnum.colorToEnum(intent.getStringExtra("COLOR")!!)
+        val typeFilter = intent.getIntExtra("FILTER", 1)
+
+        val filter =
+            AdversimentFilterDto(location, priceMin, priceMax, quality, category, color, typeFilter)
+
+        api.search(idUser, title, filter)
+            .enqueue(object : Callback<List<AllAdversimentsAndLikeDtoResponse>> {
+                override fun onResponse(
+                    call: Call<List<AllAdversimentsAndLikeDtoResponse>>,
+                    response: Response<List<AllAdversimentsAndLikeDtoResponse>>
+                ) {
+                    if (response.isSuccessful) {
+                        val adversiments = response.body()
+                        if (adversiments.isNullOrEmpty()) {
+                            binding.imgError.visibility = View.VISIBLE
+                            binding.errorMessage.visibility = View.VISIBLE
+                        } else {
+                            binding.tvQtdAdversiments.text =
+                                "${adversiments.size} anúnios encontrados"
+                            initRecyclerView(adversiments)
+                        }
+                    }
+                }
+
+                override fun onFailure(
+                    call: Call<List<AllAdversimentsAndLikeDtoResponse>>,
+                    t: Throwable
+                ) {
+                    Toast.makeText(baseContext, t.message, Toast.LENGTH_LONG).show()
+                }
+            })
+    }
+}
--- app/src/main/java/com/example/letsbuy/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/letsbuy/ui/home/HomeFragment.kt
@@ -1,18 +1,24 @@
 package com.example.letsbuy.ui.home
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.letsbuy.SearchActivity
 import com.example.letsbuy.adapter.AdapterProduto
 import com.example.letsbuy.api.Rest
 import com.example.letsbuy.databinding.FragmentHomeBinding
+import com.example.letsbuy.dto.AdversimentFilterDto
 import com.example.letsbuy.dto.AllAdversimentsAndLikeDtoResponse
-import com.example.letsbuy.service.AdversimentService
+import com.example.letsbuy.service.SearchService
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -26,23 +32,128 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(layoutInflater)
         binding.progressBar.visibility = View.VISIBLE
 
+        Log.w("TESTE", "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
         val pref = context?.getSharedPreferences("AUTH", AppCompatActivity.MODE_PRIVATE)
-        val id = pref?.getString("ID", null)?.toLong()
-        retrieveAdversiment(id!!)
+        var id = pref?.getString("ID", null)?.toLong()
+
+        binding.editMobileNo.setOnEditorActionListener { _, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE ||
+                actionId == EditorInfo.IME_ACTION_SEARCH ||
+                (event != null && event.action == KeyEvent.ACTION_DOWN &&
+                        event.keyCode == KeyEvent.KEYCODE_ENTER)
+            ) {
+                if (binding.editMobileNo.text.isNullOrEmpty()) {
+                    initFilter(id!!)
+                    false
+                } else {
+                    val title = binding.editMobileNo.text.toString()
+                    search(id!!, title)
+                    return@setOnEditorActionListener true
+                }
+            }
+            false
+        }
+
+        binding.imgFashionAcessories.setOnClickListener {
+            val filter = Intent(context, SearchActivity::class.java)
+            filter.putExtra("CATEGORY", "Acessórios")
+            startActivity(filter)
+        }
+
+        binding.imgElectronis.setOnClickListener {
+            val filter = Intent(context, SearchActivity::class.java)
+            filter.putExtra("CATEGORY", "Eletrônicos")
+            startActivity(filter)
+        }
+
+        binding.imgHouseDeoratioNn.setOnClickListener {
+            val filter = Intent(context, SearchActivity::class.java)
+            filter.putExtra("CATEGORY", "Casa e decoração")
+            startActivity(filter)
+        }
+
+        binding.imgBooks.setOnClickListener {
+            val filter = Intent(context, SearchActivity::class.java)
+            filter.putExtra("CATEGORY", "Livros")
+            startActivity(filter)
+        }
+
+        binding.imgVehicles.setOnClickListener {
+            val filter = Intent(context, SearchActivity::class.java)
+            filter.putExtra("CATEGORY", "Veículos")
+            startActivity(filter)
+        }
+
+        binding.imgSportsLeisure.setOnClickListener {
+            val filter = Intent(context, SearchActivity::class.java)
+            filter.putExtra("CATEGORY", "Esporte e lazer")
+            startActivity(filter)
+        }
+
+        binding.imgPets.setOnClickListener {
+            val filter = Intent(context, SearchActivity::class.java)
+            filter.putExtra("CATEGORY", "Pets")
+            startActivity(filter)
+        }
+
+        binding.imgChildren.setOnClickListener {
+            val filter = Intent(context, SearchActivity::class.java)
+            filter.putExtra("CATEGORY", "Bebês e crianças")
+            startActivity(filter)
+        }
+
+        initFilter(id!!)
+
         return binding.root
     }
 
     private fun initRecyclerView(listProduct: List<AllAdversimentsAndLikeDtoResponse>) {
+        Log.w("List init", listProduct.toString())
         binding.recyclerViewProdutos.layoutManager = LinearLayoutManager(context)
         binding.recyclerViewProdutos.setHasFixedSize(true)
         binding.recyclerViewProdutos.adapter = AdapterProduto(listProduct)
     }
 
-    private fun retrieveAdversiment(id: Long) {
-        val teste: List<AllAdversimentsAndLikeDtoResponse> = listOf()
+    fun search(id: Long, title: String) {
+        val api = Rest.getInstance().create(SearchService::class.java)
+        api.searchHome(id, title)
+            .enqueue(object : Callback<List<AllAdversimentsAndLikeDtoResponse>> {
+                override fun onResponse(
+                    call: Call<List<AllAdversimentsAndLikeDtoResponse>>,
+                    response: Response<List<AllAdversimentsAndLikeDtoResponse>>
+                ) {
+                    binding.progressBar.visibility = View.GONE
+                    if (response.isSuccessful) {
+                        val adversiment = response.body()
+                        if (adversiment.isNullOrEmpty()) {
+                            binding.imgError.visibility = View.VISIBLE
+                            binding.errorMessage.visibility = View.VISIBLE
+                        } else {
+                            binding.tvFilter.text = "${adversiment.size} anúnios encontrados"
+                            initRecyclerView(adversiment)
+                        }
+                    } else {
+                        val message = "Requisição nao aceita"
+                        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+                    }
+                }
 
-        val api = Rest.getInstance().create(AdversimentService::class.java)
-        api.retrieveAdversiment(id)
+                override fun onFailure(
+                    call: Call<List<AllAdversimentsAndLikeDtoResponse>>,
+                    t: Throwable
+                ) {
+                    Toast.makeText(context, t.message, Toast.LENGTH_LONG).show()
+                }
+            })
+    }
+
+    fun initFilter(idUser: Long) {
+        val api = Rest.getInstance().create(SearchService::class.java)
+
+        val filter =
+            AdversimentFilterDto(null, null, null, null, null, null, 1)
+
+        api.search(idUser, null, filter)
             .enqueue(object : Callback<List<AllAdversimentsAndLikeDtoResponse>> {
                 override fun onResponse(
                     call: Call<List<AllAdversimentsAndLikeDtoResponse>>,
@@ -64,7 +175,6 @@ class HomeFragment : Fragment() {
                     call: Call<List<AllAdversimentsAndLikeDtoResponse>>,
                     t: Throwable
                 ) {
-                    binding.progressBar.visibility = View.GONE
                     Toast.makeText(context, t.message, Toast.LENGTH_LONG).show()
                 }
             })
